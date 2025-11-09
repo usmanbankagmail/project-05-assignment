716332d525ff00b10b1520e728cd3cf4cad0e43d
usmanbankagmail <usmanbanka@gmail.com>
2025-11-09 14:50:52 +0500

    moved app.js again to root folder..lets see if vercel now deploys successfully or not

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import productRoutes from "../routes/productRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
 /*Sir's Style of Using Middleware ... 
 
