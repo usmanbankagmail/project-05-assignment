42d1d0d806ec62881dfea94797e28cb81c9a4eaa
usmanbankagmail <usmanbanka@gmail.com>
2025-11-09 13:42:38 +0500

    Updated code pushed to github for it to work on vercel

--- app.js
+++ app.js
@@ -3,6 +3,60 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 
+/*Sir's Style of Using Middleware ... 
+
+let basicMiddleware = (req, res, next) => {
+    console.log('middleware call');
+    next();
+}
+
+let authenticate = (req, res, next) => {
+    if(isLogin){
+        next();
+    }
+    res.send('you need to login')
+}
+
+app.use(express.json())  //application-level
+app.use(express.urlencoded())  //application-level
+app.use(express.text())  //application-level
+app.use(morgan('short'));
+// event-driven
+
+app.get('/', basicMiddleware, (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/products',authenticate, (req,res)=>{
+    res.send(['product1','product2'])
+})
+
+app.get('/todos',basicMiddleware,authenticate, (req,res)=>{
+    res.send(['todo1','todo2'])
+})
+
+
+app.use('/users', userRoutes)
+
+
+// to show the form
+app.get('/login', (req, res) => {
+    res.send('<form method=POST action=/login><input type=text name=username><input type=number name=age><input type=submit></form>')
+})
+
+// submitting the form
+app.post('/login', (req, res) => {
+    console.log(`the form body : `, req.body)
+    res.send('data has been recieved by the server')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+*/
+
+
+
 dotenv.config();
 
 const app = express();
